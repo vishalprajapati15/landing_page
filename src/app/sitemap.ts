13808fc6497d750892bd9d2389cf060a27558236
maxpointdigitalservice <maxpointb2b@gmail.com>
2026-08-18 20:30:32 +0530

    fix sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://maxpointdev-mu.vercel.app/";
+  const baseUrl = "https://maxpointdev-mu.vercel.app";
 
   return [
     {
